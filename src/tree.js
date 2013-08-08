504663707362a7a48356b90e979f078b98b39593
Hack Reactor Students <github@hackreactor.com>
2013-08-08 16:46:18 -0700

    Added constructor, addChild, and contains()

--- src/tree.js
+++ src/tree.js
@@ -1,15 +1,34 @@
 var makeTree = function(){
   var newTree = {};
   newTree.value = undefined;
-  newTree.children = undefined;
+  newTree.children = [];
 
+  for (var key in treeMethods) {
+    newTree[key] = treeMethods[key];
+  }
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  tempTree = makeTree();
+  tempTree.value = value;
+  this.children.push(tempTree);
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(data){
+  var doesContain = false;
+  var treeCrawler = function(node){
+    if (node.value === data) {
+      doesContain = true;
+    }
+    if (node.children.length) {
+      for (var i = 0; i < node.children; i++){
+        treeCrawler(node.next);
+      }
+    }
+  };
+  treeCrawler(list.head);
+  return doesContain;
 };
