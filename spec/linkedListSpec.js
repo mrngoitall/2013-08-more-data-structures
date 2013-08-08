bb58b0db13962eb633d0ecb25d5ababfce63c93c
Hack Reactor Students <github@hackreactor.com>
2013-08-08 15:34:46 -0700

    Added linkedList tests

--- spec/linkedListSpec.js
+++ spec/linkedListSpec.js
@@ -17,4 +17,35 @@ describe("linkedList", function() {
   });
 
   // add more tests here to test the functionality of linkedList
+
+  it("should be able to add a value to tail", function() {
+    linkedList.addToTail('a');
+    expect(linkedList.removeHead()).toEqual('a');
+  });
+
+  it("should have meaningful head and tail properties", function() {
+    linkedList.addToTail('a');
+    linkedList.addToTail('b');
+
+    expect(linkedList.head.value).toEqual('a');
+    expect(linkedList.tail.value).toEqual('b');
+  });
+
+  it("should return items in the proper order", function() {
+    linkedList.addToTail('a');
+    linkedList.addToTail('b');
+    linkedList.addToTail('c');
+
+    expect(linkedList.removeHead()).toEqual('a');
+  });
+
+  it("should be able to find values in the list with contains()", function() {
+    linkedList.addToTail('a');
+    linkedList.addToTail('b');
+    linkedList.addToTail('c');
+
+    expect(linkedList.contains('b')).toEqual(true);
+    expect(linkedList.contains('d')).toEqual(false);
+  });
+
 });
