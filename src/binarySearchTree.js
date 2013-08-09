3764a9b65a65ac6b4341c796c118135bc86ea322
Hack Reactor Students <github@hackreactor.com>
2013-08-09 15:21:52 -0700

    Added insert() to binarySearchTree

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -14,7 +14,28 @@ var makeBinarySearchTree = function(){
 var binarySearchTreeMethods = {};
 
 binarySearchTreeMethods.insert = function(value) {
-
+  if (!this.value) {
+    this.value = value;
+  } else {
+    var newNode = makeBinarySearchTree;
+    newNode.value = value;
+    var binaryTreeCrawler = function(node) {
+      if (newNode.value < node.value) {
+        if (node.left === null) {
+          node.left = newNode;
+        } else {
+          binaryTreeCrawler(node.left);
+        }
+      } else if (newNode.value > node.value) {
+        if (node.right === null) {
+          node.right = newNode;
+        } else {
+          binaryTreeCrawler(node.right);
+        }
+      }
+    };
+    binaryTreeCrawler(this);
+  }
 };
 
 binarySearchTreeMethods.contains = function(value) {
