1bb9672fc278c81f92edfdd1932d4764d0f736af
Andrew Delikat <andrewdelikat@gmail.com>
2013-08-09 09:59:08 -0700

    Added skeleton for binarySearchTree

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,26 @@
 var makeBinarySearchTree = function(){
+  var newBinarySearchTree = {}
+  newBinarySearchTree.value = null;
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+
+  for (var key in treeMethods) {
+    newTree[key] = treeMethods[key];
+  }
+
+  return newTree;
+};
+
+var binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(value) {
+
+};
+
+binarySearchTreeMethods.contains = function(value) {
+  
+};
+
+binarySearchTreeMethods.depthFirstLog = function(callback) {
+  
 };
