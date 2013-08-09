057b50f6308c4168de69e85512bf083867e86d19
Hack Reactor Students <github@hackreactor.com>
2013-08-09 15:05:40 -0700

    Added skeleton code for binarySearchTree

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,26 @@
 var makeBinarySearchTree = function(){
+  var newBinarySearchTree = {};
+  newBinarySearchTree.value = null;
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+
+  for (var key in binarySearchTreeMethods) {
+    newBinarySearchTree[key] = binarySearchTreeMethods[key];
+  }
+
+  return newBinarySearchTree;
+};
+
+var binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(value) {
+
+};
+
+binarySearchTreeMethods.contains = function(value) {
+  
+};
+
+binarySearchTreeMethods.depthFirstLog = function(callback) {
+  
 };
