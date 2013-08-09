ffb68ce1e90115f384b8c36c75c33938dfdd604b
Hack Reactor Students <github@hackreactor.com>
2013-08-08 17:34:48 -0700

    Added add, contains, and remove tests

--- spec/setSpec.js
+++ spec/setSpec.js
@@ -10,4 +10,19 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+
+  it("should be able to add string values to the set and use contains to find them", function () {
+    set.add("Hello");
+    expect(set.contains("Hello")).toEqual(true);
+    expect(set.contains("Goodbye")).toEqual(false);
+  });
+
+  it("should be able to remove string values from the set without changing other values", function () {
+    set.add("Hello");
+    set.add("Hi");
+    set.remove("Hello");
+    set.remove("Goodbye");
+    expect(set.contains("Hello")).toEqual(false);
+    expect(set.contains("Hi")).toEqual(true);
+  });
 });
