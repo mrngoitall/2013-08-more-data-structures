6f2054b0c5d2c5c1330a932a0a5a15b6e640f178
Hack Reactor Students <github@hackreactor.com>
2013-08-09 17:01:09 -0700

    Made findClosest tree algorithm faster/smarter

--- src/bsttester.js
+++ src/bsttester.js
@@ -1,10 +1,10 @@
 var binarySearchTreeTestSetup = function(tree, array) {
   for (var i = 0; i < 1000000; i++) {
-    var randomNo = Math.floor(Math.random() * 1000000)
+    var randomNo = Math.floor(Math.random() * 1000000);
     tree.insert(randomNo);
     array[i] = randomNo;
   }
-}
+};
 
 var findClosest = function(tree, value) {
   var closest = tree.value;
@@ -12,22 +12,25 @@ var findClosest = function(tree, value) {
     if (Math.abs(node.value - value) < (Math.abs(closest - value))) {
       closest = node.value;
     }
-    node.left && binaryTreeCrawler(node.left);
-    node.right && binaryTreeCrawler(node.right);
-  }
+    if (value < node.value && node.left) {
+      binaryTreeCrawler(node.left);
+    } else if (value > node.value && node.right) {
+      binaryTreeCrawler(node.right);
+    }
+  };
   binaryTreeCrawler(tree);
   return closest;
-}
+};
 
 var findClosestArray = function (array, value) {
-  var closest = array[0]
+  var closest = array[0];
   for (var i = 0; i < array.length; i++) {
     if (Math.abs(array[i] - value) < (Math.abs(closest - value))) {
       closest = array[i];
     }
   }
   return closest;
-}
+};
 
 var myTree = makeBinarySearchTree();
 var myArray = [];
