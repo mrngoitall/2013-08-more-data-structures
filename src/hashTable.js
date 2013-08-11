1979b23aa009755e5c965cad700e7638d998c868
Hack Reactor Students <github@hackreactor.com>
2013-08-10 17:24:58 -0700

    Cleaned up HashTable

--- src/hashTable.js
+++ src/hashTable.js
@@ -17,28 +17,24 @@ HashTable.prototype.insert = function(k, v, limit, storage){
   limit = limit || this._limit;
   newStorage = storage || this._storage;
   var i = getIndexBelowMaxForKey(k, limit);
-  var thisBucket = newStorage.get(i);
-  var updated = false;
-  if (!thisBucket) {
-    thisBucket = [];
-    thisBucket[0] = k;
-    thisBucket[1] = v;
-    if (!storage) {
-      this._size++;
+  var thisBucket = newStorage.get(i) || [];
+  var pair = [];
+
+  _(thisBucket).each(function(thisPair) {
+    if (thisPair[0] === k) {
+      pair = thisPair;
+      thisPair[1] = v;
     }
-  } else {
-    for (var x = 0; x < thisBucket.length; x += 2) {
-      if (thisBucket[x] === k) {
-        thisBucket[x + 1] = v;
-        updated = true;
-      }
-    }
-     if (!updated) {
-       thisBucket.push(k);
-       thisBucket.push(v);
-     }
+  });
+
+  if (!pair.length) {
+    pair = [k, v];
+    thisBucket.push(pair);
+    !storage && this._size++;
   }
   newStorage.set(i, thisBucket);
+
+  // Rebalance tree if more than 75% is used
   if (this._size > (limit * 0.75)) {
     this.reindex(limit * 2);
   }
@@ -46,30 +42,27 @@ HashTable.prototype.insert = function(k, v, limit, storage){
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var thisBucket = this._storage.get(i);
+  var thisBucket = this._storage.get(i) || [];
   var value;
-  if (thisBucket) {
-    for (var x = 0; x < thisBucket.length; x += 2) {
-      if (thisBucket[x] === k) {
-        value = thisBucket[x + 1];
-      }
-    }
-  }
+  _(thisBucket).each(function(thisPair) {
+    if (thisPair[0] === k)
+      value = thisPair[1];
+  });
   return value;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var thisBucket = this._storage.get(i);
-    for (var x = 0; x < thisBucket.length; x += 2) {
-      if (thisBucket[x] === k) {
-        thisBucket.splice(x, 2);
-      }
-  }
+  _(thisBucket).each(function(thisPair, pairIndex) {
+    thisPair[0] === k && thisBucket.splice(pairIndex,1);
+  });
   if (thisBucket.length === 0) {
     this._size--;
   }
   this._storage.set(i, thisBucket);
+
+  // Rebalance tree if less than 25% is used
   if (this._size < (this._limit * 0.25)) {
     this.reindex(this._limit / 2);
   }
@@ -79,11 +72,9 @@ HashTable.prototype.reindex = function(newStorageLimit) {
   var htable = this;
   var newHTable = makeLimitedArray(newStorageLimit);
   this._storage.each(function(thisBucket, key, collection) {
-    if (thisBucket) {
-      for (var x = 0; x < thisBucket.length; x += 2) {
-        htable.insert(thisBucket[x],thisBucket[x+1],newStorageLimit,newHTable);
-      }
-    }
+    thisBucket && _(thisBucket).each(function(thisPair) {
+      htable.insert(thisPair[0],thisPair[1],newStorageLimit,newHTable);
+    });
   });
   this._limit = newStorageLimit;
   this._storage = newHTable;
